19ecfd6eb0b746fcdaad8a7b1cacdd3ca4e9d875
GOWxx <sunbowen523@gmail.com>
2024-03-08 00:21:41 +0800

    feat: ONLY use plugin is the right way

--- app/root.tsx
+++ app/root.tsx
@@ -6,17 +6,6 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import { LinksFunction } from "@remix-run/node";
-
-import rdtStylesheet from "remix-development-tools/index.css?url";
-
-export const links: LinksFunction = () => [
-  // export the stylesheet in development only
-  ...(process.env.NODE_ENV === "development"
-    ? [{ rel: "stylesheet", href: rdtStylesheet }]
-    : []),
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -35,15 +24,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-function App() {
+export default function App() {
   return <Outlet />;
 }
-
-let AppExport = App;
-
-if (process.env.NODE_ENV === "development") {
-  const { withDevTools } = await import("remix-development-tools");
-  AppExport = withDevTools(AppExport);
-}
-
-export default AppExport;
